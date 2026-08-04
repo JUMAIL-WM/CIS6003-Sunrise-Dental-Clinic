23dcdab02f6dcfa36fe7ae1841b0a82af78bad6f
MOHAMMED JUMAIL <jumailwm@gmail.com>
2026-08-04 19:59:08 +0530

    Initial commit

---
+++ src/sunrisedentalsystem/DBConnection.java
@@ -0,0 +1,14 @@
+package sunrisedentalsystem;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Future_Mind
+ */
+public class DBConnection {
+    
+}
