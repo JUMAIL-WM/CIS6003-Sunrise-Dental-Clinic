95dc3695b3069abe344ba833828174ade6979e1b
MOHAMMED JUMAIL <jumailwm@gmail.com>
2026-08-10 21:35:14 +0530

    Add Database Connection and Main entry point
    Add Database Connection and Main entry point

--- src/sunrisedentalsystem/DBConnection.java
+++ src/sunrisedentalsystem/DBConnection.java
@@ -1,5 +1,7 @@
 package sunrisedentalsystem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +11,32 @@ package sunrisedentalsystem;
  *
  * @author Future_Mind
  */
-public class DBConnection {
-    
+public class DBConnection 
+{
+    private static DBConnection instance;
+    private Connection connection;
+
+    private DBConnection() 
+    {
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sunrise_dental_db", "root", "");
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static DBConnection getInstance() 
+    {
+        if (instance == null) instance = new DBConnection();
+        return instance;
+    }
+
+    public Connection getConnection() 
+    {
+        return connection;
+    }
 }
--- src/sunrisedentalsystem/Main.java
+++ src/sunrisedentalsystem/Main.java
@@ -4,10 +4,19 @@
  */
 package sunrisedentalsystem;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
 /**
  *
  * @author Future_Mind
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        java.awt.EventQueue.invokeLater(() -> new LoginPage().setVisible(true));
+    }
 }
